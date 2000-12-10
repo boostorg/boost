8328929b859ba4c5ef1892872d10433609e3aa89
Jeremy Siek <jeremy.siek@gmail.com>
2000-12-10 17:54:14 +0000

    removed the not-really-needed test files of the concept check tests

--- libs/regrtest.py
+++ libs/regrtest.py
@@ -247,8 +247,6 @@ if  program_arg=="*":
 #  compile( "compose/compose1.cpp" )
   compile( "concept_check/concept_check_test.cpp")
   compile( "concept_check/class_concept_check_test.cpp")
-  compile( "concept_check/stl_concept_check.cpp")
-  compile( "concept_check/stl_concept_covering.cpp")
   compile( "concept_check/concept_check_fail_expected.cpp")
   compile( "concept_check/class_concept_check_fail_expected.cpp")
   compile( "functional/function_test.cpp" )
