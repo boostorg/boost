dc70745280d376e1400791dcbf25ede7db1c4489
Beman Dawes <bdawes@acm.org>
2004-01-31 16:08:31 +0000

    chg for SunOS cc 5.5 (Baptiste Lepilleur)

--- tools/regression/compiler_status.cpp
+++ tools/regression/compiler_status.cpp
@@ -106,7 +106,7 @@ namespace
       string bookmark( line.substr( pos+1 ) );
 
 //      std::cout << "inserting \"" << key << "\",\"" << bookmark << "\"\n";
-      notes.insert( std::make_pair( key, bookmark ) );
+      notes.insert( notes_map::value_type( key, bookmark ) );
     }
   }
 
