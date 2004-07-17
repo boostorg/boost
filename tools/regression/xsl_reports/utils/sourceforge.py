c1f011a1bbb789a72ef264e82ee261c9aed8d4d0
Aleksey Gurtovoy <agurtovoy@meta-comm.com>
2004-07-17 15:12:54 +0000

    more bug fixes
    [SVN r23689]

--- tools/regression/xsl_reports/utils/sourceforge.py
+++ tools/regression/xsl_reports/utils/sourceforge.py
@@ -3,14 +3,14 @@ import utils.checked_system
 import os
 import sys
 
-site_dir = '/home/groups/b/bo/boost/htdocs'
+site_dir = '/home/groups/b/bo/boost/htdocs/'
 
 def download( source, destination, user ):
     if sys.platform == 'win32':
         destination = os.popen( 'cygpath "%s"' % destination ).read().splitlines()[0]
 
     utils.checked_system( [ 
-          'rsync -v -r -z --progress %(user)s@shell.sourceforge.net:%(site_dir)s/%(source)s %(dest)s'
+          'rsync -v -r -z --progress %(user)s@shell.sourceforge.net:%(site_dir)s%(source)s %(dest)s'
                 % { 'user': user, 'site_dir': site_dir, 'source': source, 'dest': destination }
         ] )
 
@@ -20,7 +20,7 @@ def upload( source, destination, user ):
         source = os.popen( 'cygpath "%s"' % source ).read().splitlines()[0]
         
     utils.checked_system( [ 
-          'rsync -v -r -z --progress %(source)s %(user)s@shell.sourceforge.net:%(site_dir)s/%(dest)s'
+          'rsync -v -r -z --progress %(source)s %(user)s@shell.sourceforge.net:%(site_dir)s%(dest)s'
                 % { 'user': user, 'site_dir': site_dir, 'source': source, 'dest': destination }
         ] )
 
