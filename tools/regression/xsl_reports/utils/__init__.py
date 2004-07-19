7ab745f75232f1df9e69621f78bcf3d9f28be58a
Aleksey Gurtovoy <agurtovoy@meta-comm.com>
2004-07-19 09:17:07 +0000

    runner/regression.py initial checkin

--- tools/regression/xsl_reports/utils/__init__.py
+++ tools/regression/xsl_reports/utils/__init__.py
@@ -1,10 +1,13 @@
 
 from accept_args import *
 from char_translation_table import *
+from check_existance import *
 from checked_system import *
+from http_get import *
 from libxslt import *
 from log import *
 from makedirs import *
+from rmtree import *
 from tar import *
 
 import sourceforge
