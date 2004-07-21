30985346836e4fc7300ba18db6da89bf73b31dd9
Aleksey Gurtovoy <agurtovoy@meta-comm.com>
2004-07-21 02:47:51 +0000

    remove deleted modules from __init__.py

--- tools/regression/xsl_reports/utils/__init__.py
+++ tools/regression/xsl_reports/utils/__init__.py
@@ -3,11 +3,9 @@ from accept_args import *
 from char_translation_table import *
 from check_existance import *
 from checked_system import *
-from http_get import *
 from libxslt import *
 from log import *
 from makedirs import *
-from rmtree import *
 from tar import *
 
 import sourceforge
