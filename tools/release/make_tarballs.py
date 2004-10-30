0b70cbc66b67dddcc71591032c24bf1662bf253b
Misha Bergal <mbergal@meta-comm.com>
2004-10-30 13:12:37 +0000

    Some improvements

--- tools/release/make_tarballs.py
+++ tools/release/make_tarballs.py
@@ -52,13 +52,29 @@ def build_win( release_version, cvs_tag, sf_user, temp_dir ):
     os.system( "7z a -r -tzip %s %s\* > %s" % ( os.path.splitext( zip_name )[0], "boost_%s" % release_version, zip_name + ".log" ) )
     return [ os.path.abspath( zip_name ) ]
 
+def correct_permissions( path ):
+    print "    Correcting permissions"
+    for i in os.walk( path ):
+        for f in i[2]:
+            if os.path.splitext( f )[1] in ( ".css", ".hpp", ".cpp",\
+                                             ".html", ".htm", ".rst", \
+                                             ".pdf", ".xml", ".png",\
+                                             ".jpg", ".vcproj", ".pattern2", \
+                                             ".jam", ".bat", ".sty", ".diff" ) \
+               or os.path.basename( f ).lower() in ( "jamfile", "todo", "makefile", "jamrules", "gnumakefile" ):
+                print os.path.join( i[0], f )
+                os.system( "chmod a=r %s" % os.path.join( i[0], f ) )
+    
 def build_unix( release_version, cvs_tag, sf_user, temp_dir ):
     print "Preparing unix .gz and .bz2"
     temp_unix = make_temp_platform( temp_dir, "unix" )
     os.chdir( temp_unix )
 
     cvs_export( sf_user, cvs_tag, release_version, "bash -c \"%s\"" )
-        
+    correct_permissions( "." )
+
+    
+    
     print "    Making .gz"
     gz_archive_name = "boost_%s" % release_version + ".tar.gz"
     
@@ -77,8 +93,8 @@ def main():
     start_dir = os.getcwd()
     ( release_version, cvs_tag, sf_user, temp_dir ) = accept_args( sys.argv[ 1: ] )
 
-    archives = build_unix( release_version, cvs_tag, sf_user, temp_dir )
-##    build_win( release_version, cvs_tag, sf_user, temp_dir )\              +  
+    archives = build_win( release_version, cvs_tag, sf_user, temp_dir )\
+               + build_unix( release_version, cvs_tag, sf_user, temp_dir )
 
     os.chdir( start_dir )
     for archive in archives:
