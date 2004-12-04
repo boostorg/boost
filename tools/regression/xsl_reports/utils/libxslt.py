44e1cbad4e23f2a5d8c4feef91750ee010ddc5f6
Aleksey Gurtovoy <agurtovoy@meta-comm.com>
2004-12-04 13:01:23 +0000

    usability fixes
    [SVN r26426]

--- tools/regression/xsl_reports/utils/libxslt.py
+++ tools/regression/xsl_reports/utils/libxslt.py
@@ -11,9 +11,10 @@ def libxslt( log, xml_file, xsl_file, output_file, parameters = None ):
     transform_command = transform_command + ' -o ' + '%s' % output_file
 
     if parameters is not None:
-         for i in parameters: 
-             parameters[i] = parameters[i].replace( '\\', '/' )
-             transform_command = transform_command + ' --param %s "\'%s\'" ' % ( i, parameters[ i ] )
+         for i in parameters:
+             if parameters[i]:
+                 parameters[i] = parameters[i].replace( '\\', '/' )
+                 transform_command = transform_command + ' --param %s "\'%s\'" ' % ( i, parameters[ i ] )
 
     transform_command = transform_command + ' "%s" ' % xsl_file
     transform_command = transform_command + ' "%s" ' % xml_file
