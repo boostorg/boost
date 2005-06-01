ecea91b82017ae7657f3332ebef19c6d3616ad5d
Beman Dawes <bdawes@acm.org>
2005-06-01 14:16:33 +0000

    Add additional tests to cover more cases

---
+++ tools/regression/test/run_fail-fail.cpp
@@ -0,0 +1,8 @@
+//  (C) Copyright Beman Dawes 2003.  Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+int main()
+{
+  return 0;
+}
