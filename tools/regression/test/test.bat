36ad43331ca12c3f527c3beccd853a060ad942b9
Beman Dawes <bdawes@acm.org>
2005-06-01 14:14:38 +0000

    Work with current bjam
    [SVN r29341]

--- tools/regression/test/test.bat
+++ tools/regression/test/test.bat
@@ -1,9 +1,10 @@
-@echo off
+set TEST_LOCATE_ROOT=d:\temp
+
 echo Begin test processing...
-bjam --dump-tests %* >bjam.log 2>&1
+bjam --dump-tests "-sALL_LOCATE_TARGET=%TEST_LOCATE_ROOT%" %* >bjam.log 2>&1
 echo Begin log processing...
-process_jam_log <bjam.log
+process_jam_log %TEST_LOCATE_ROOT% <bjam.log
 start bjam.log
 echo Begin compiler status processing...
-compiler_status %BOOST_ROOT% test_status.html test_links.html
+compiler_status --locate-root %TEST_LOCATE_ROOT% %BOOST_ROOT% test_status.html test_links.html
 start test_status.html
