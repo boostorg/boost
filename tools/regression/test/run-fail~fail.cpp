7fe7dae43bc6a44a81ba62d8a83b79dfddac0579
Beman Dawes <bdawes@acm.org>
2005-06-19 21:24:33 +0000

    Rename existing tests to clarify expected results. Add additional tests to cover more cases, including some which Phil Richards discovered were being reported incorrectly by compiler_status.cpp
    [SVN r29691]

--- tools/regression/test/run-fail~fail.cpp
+++ tools/regression/test/run-fail~fail.cpp
@@ -2,12 +2,11 @@
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <iostream>
+//  Test naming convention: the portion of the name before the tilde ("~")
+//  identifies the bjam test type. The portion after the tilde
+//  identifies the correct result to be reported by compiler_status.
 
 int main()
 {
-  std::cout << "example of output before a <note> line\n";
-  std::cout << "<note>\n";
-  std::cout << "example of output after a <note> line\n";
   return 0;
 }
--- tools/regression/test/run-fail~warn.cpp
+++ tools/regression/test/run-fail~warn.cpp
@@ -2,13 +2,19 @@
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// provoke one or more compiler warnings
+//  Test naming convention: the portion of the name before the tilde ("~")
+//  identifies the bjam test type. The portion after the tilde
+//  identifies the correct result to be reported by compiler_status.
 
-int main(int argc, char * argv[] )
+#include <iostream>
+
+int main()
 {
   short s;
   unsigned long ul;
   s = s & ul; // warning from many compilers
   if ( s == ul ) {} // warning from GCC
-  return 0;
+
+  std::cout << "example of output from a run-time failure\n";
+  return 1;
 }
