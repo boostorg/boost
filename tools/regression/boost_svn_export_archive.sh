08386d0f34ca30bd5ecfc0eee81bd9da90997207
Rene Rivera <grafikrobot@gmail.com>
2007-08-27 21:05:35 +0000

    Tarball generation script.

---
+++ tools/regression/boost_svn_export_archive.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+export PATH=/bin:/usr/bin:${PATH}
+
+timestamp=`date +%F-%H-%M-%S-%Z`
+branch=$1
+tag=boost-${1/\/*}-${timestamp}
+export_dir=boost-$$
+
+rm_c()
+{
+  while read f; do
+    rm -f ${f}
+  done
+}
+svn_export()
+{
+  svn export file:///home/subversion/boost/${branch} ${tag} \
+    | cut --bytes=6- \
+    | star -c -D -to-stdout -d artype=tar list=- 2>/dev/null \
+    | bzip2 -3 -c \
+    | tee $1 \
+    | tar -jtf - \
+    | rm_c
+}
+
+cd /tmp
+rm -rf ${export_dir}
+mkdir ${export_dir}
+cd ${export_dir}
+mkfifo out.tbz2
+svn_export out.tbz2 &
+#cat out.tbz2 > ../${tag}.tar.bz2
+cat out.tbz2
+cd /tmp
+rm -rf ${export_dir}
+
