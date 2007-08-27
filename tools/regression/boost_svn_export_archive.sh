110777659fb67fde6c921c773a273029d73b7c0c
Rene Rivera <grafikrobot@gmail.com>
2007-08-27 21:19:41 +0000

    Add copyright+BSL.

--- tools/regression/boost_svn_export_archive.sh
+++ tools/regression/boost_svn_export_archive.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#~ Copyright Redshift Software, Inc. 2007
+#~ Distributed under the Boost Software License, Version 1.0.
+#~ (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+
 export PATH=/bin:/usr/bin:${PATH}
 
 timestamp=`date +%F-%H-%M-%S-%Z`
