d8497c71ce90caa460123843e055b0d7a50b1e4c
Rene Rivera <grafikrobot@gmail.com>
2007-09-21 17:14:13 +0000

    Add svn_info.txt file to annotate the revision exported.

--- tools/regression/boost_svn_export_archive.sh
+++ tools/regression/boost_svn_export_archive.sh
@@ -8,34 +8,63 @@ export PATH=/bin:/usr/bin:${PATH}
 
 timestamp=`date +%F-%H-%M-%S-%Z`
 branch=$1
+revision=`svn info file:///home/subversion/boost/${branch} | grep '^Revision:' | cut --byte=11-`
 tag=boost-${1/\/*}-${timestamp}
 export_dir=boost-$$
 
+# Remove files as listed in stdin, the assumption is that processing
+# of the file is complete and can be removed.
 rm_c()
 {
   while read f; do
     rm -f ${f}
   done
 }
+# Generate the export file tree, and incrementally output the files
+# created.
 svn_export()
 {
-  svn export file:///home/subversion/boost/${branch} ${tag} \
+  svn export -r ${revision} file:///home/subversion/boost/${branch} ${tag}
+  echo "Revision: ${revision}" > ${tag}/svn_info.txt
+  echo "---- ${tag}/svn_info.txt"
+}
+# Create the archive incrementally, deleting files as we are done
+# adding them to the archive.
+make_archive()
+{
+  svn_export \
     | cut --bytes=6- \
     | star -c -D -to-stdout -d artype=tar list=- 2>/dev/null \
-    | bzip2 -3 -c \
+    | bzip2 -6 -c \
     | tee $1 \
     | tar -jtf - \
     | rm_c
 }
 
-cd /tmp
-rm -rf ${export_dir}
-mkdir ${export_dir}
-cd ${export_dir}
-mkfifo out.tbz2
-svn_export out.tbz2 &
-#cat out.tbz2 > ../${tag}.tar.bz2
-cat out.tbz2
-cd /tmp
-rm -rf ${export_dir}
+run()
+{
+  cd /tmp
+  rm -rf ${export_dir}
+  mkdir ${export_dir}
+  cd ${export_dir}
+  mkfifo out.tbz2
+  make_archive out.tbz2 &
+  cat out.tbz2
+  cd /tmp
+  rm -rf ${export_dir}
+}
 
+run_debug()
+{
+  rm -rf ${export_dir}
+  mkdir ${export_dir}
+  cd ${export_dir}
+  mkfifo out.tbz2
+  make_archive out.tbz2 &
+  cat out.tbz2 > ../${tag}.tar.bz2
+  cd ..
+  rm -rf ${export_dir}
+}
+
+run
+#run_debug
