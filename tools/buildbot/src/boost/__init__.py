eb75725a79c89c9c7a5fff19ec7799535cb562ff
Rene Rivera <grafikrobot@gmail.com>
2007-11-25 17:06:17 +0000

    Move buildbot support to sandbox until we decide what to do with it.

--- tools/buildbot/src/boost/__init__.py
+++
@@ -1,4 +0,0 @@
-#~ Copyright Redshift Software, Inc. 2006
-#~ Distributed under the Boost Software License, Version 1.0. 
-#~ (See accompanying file LICENSE_1_0.txt or copy at 
-#~ http://www.boost.org/LICENSE_1_0.txt)
--- tools/buildbot/src/boost/bot/__init__.py
+++
@@ -1,9 +0,0 @@
-
-# Copyright Redshift Software, Inc. 2005-2007
-#
-# Distributed under the Boost Software License, Version 1.0. 
-# (See accompanying file LICENSE_1_0.txt or copy at 
-# http://www.boost.org/LICENSE_1_0.txt)
-
-modified = '$Date: 2007-05-09 10:49:32 -0500 (Wed, 09 May 2007) $'
-revision = '$Revision: 37651 $'
