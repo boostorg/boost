cf7cdc927af133cfe07af52874d21555a643753b
Beman Dawes <bdawes@acm.org>
2008-10-20 13:42:11 +0000

    Get ready for 1.37.0; build docs only in the posix tree, then copy to windows tree.
    [SVN r49399]

---
+++ tools/release/snapshot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# © Copyright 2008 Beman Dawes
+# Distributed under the Boost Software License, Version 1.0.
+# See http://www.boost.org/LICENSE_1_0.txt
+
+./snapshot_posix.sh
+./snapshot_windows.sh
+./inspect.sh
