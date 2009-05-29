6be993aff33d91a1fde64e8c62dffd168e33e6b0
Beman Dawes <bdawes@acm.org>
2009-05-29 20:32:06 +0000

    Initial commit
    [SVN r53409]

---
+++ tools/release/inspect_trunk.bat
@@ -0,0 +1,34 @@
+rem Inspect Trunk
+rem Copyright Beman Dawes 2008, 2009
+
+rem Distributed under the Boost Software License, Version 1.0.
+rem See http://www.boost.org/LICENSE_1_0.txt
+
+echo Must be run in directory containing svn checkout of trunk
+
+echo Update trunk working copy...
+svn up --non-interactive
+pushd tools\inspect\build
+echo Build inspect program...
+bjam
+popd
+echo Inspect...
+dist\bin\inspect >%TEMP%\trunk_inspect.html
+
+echo Create ftp script...
+pushd %TEMP%
+copy user.txt inspect.ftp
+echo dir >>inspect.ftp
+echo binary >>inspect.ftp
+echo put trunk_inspect.html >>inspect.ftp
+echo dir >>inspect.ftp
+echo mdelete inspect-trunk.html >>inspect.ftp
+echo rename trunk_inspect.html inspect-trunk.html >>inspect.ftp
+echo dir >>inspect.ftp
+echo bye >>inspect.ftp
+
+echo Run ftp script...
+ftp -n -i -s:inspect.ftp boost.cowic.de
+popd
+
+echo Inspect script complete
