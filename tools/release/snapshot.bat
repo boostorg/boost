d5b225b661a313344ed87c83086ba2f15d957a8f
Beman Dawes <bdawes@acm.org>
2009-10-04 15:37:30 +0000

    Daily snapshot: add local trunk svn up

--- tools/release/snapshot.bat
+++ tools/release/snapshot.bat
@@ -10,6 +10,9 @@ rem  Must be run in a directory devoted to boost release snapshots
 
 echo Using %BOOST_TRUNK% as boost trunk
 time /t
+pushd %BOOST_TRUNK%
+svn up
+popd
 call %BOOST_TRUNK%\tools\release\snapshot_download_docs.bat
 time /t
 call %BOOST_TRUNK%\tools\release\snapshot_posix.bat
