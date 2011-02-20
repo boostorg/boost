dcdd59f51b2caa289b501f3f43d7a08b58ead9ef
Beman Dawes <bdawes@acm.org>
2011-02-20 12:27:21 +0000

    Make snapshots more robust by doing early directory deletes
    [SVN r69072]

--- tools/release/snapshot.bat
+++ tools/release/snapshot.bat
@@ -8,6 +8,16 @@ rem  See http://www.boost.org/LICENSE_1_0.txt
 
 rem  Must be run in a directory devoted to boost release snapshots
 
+echo Remove residue from prior runs...
+rem rmdir commands seem to finish before the deletes are necessarily complete.
+rem This can occasionally cause subsequent commands to fail because they expect
+rem the directory to be gone or empty. snapshot_posix and snapshot_windows
+rem are affected. Fix is to run rmdir here so that deletes are complete
+rem by the time snapshots are run.
+rmdir /s /q posix >nul
+rmdir /s /q windows >nul
+time /t
+
 echo Using %BOOST_TRUNK% as boost trunk
 time /t
 pushd %BOOST_TRUNK%
