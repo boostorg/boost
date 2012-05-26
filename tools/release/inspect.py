8f8f0f97eb202bf28e1742e14ed6c84b4c359a55
Beman Dawes <bdawes@acm.org>
2012-05-26 17:54:27 +0000

    inspect.py: Add command line arguments. Create temp directory if needed. Add ftp results to ftp site.

--- tools/release/inspect.py
+++ tools/release/inspect.py
@@ -1,4 +1,4 @@
-# Inspect Trunk
+# Inspect Boost source tree and upload results to ftp site
 # Copyright Beman Dawes 2008, 2009, 2012
 
 # Distributed under the Boost Software License, Version 1.0.
@@ -7,6 +7,8 @@
 import os
 import os.path
 import subprocess
+from ftplib import FTP
+import argparse
 
 def cmnd(x):  # execute a command
   rc = subprocess.call(x)
@@ -14,11 +16,27 @@ def cmnd(x):  # execute a command
     print 'aborting command:'
     print x
     exit(rc)
+    
+argpsr = argparse.ArgumentParser(
+  description='Inspect Boost source tree and upload results to ftp site',
+  epilog='Example: python inspect.py --user foo --pass bar --file inspect-trunk.html')
+argpsr.add_argument('--host', help='URL of ftp site. Default: boost.cowic.de',
+                    default='boost.cowic.de')
+argpsr.add_argument('--user', help='User name for ftp site', required=True)
+argpsr.add_argument('--password', help='Password for ftp site', required=True)
+argpsr.add_argument('--filename', help='Filename for ftp site.', required=True)
+args = argpsr.parse_args()
+filepath = 'temp/' + args.filename  
 
 if not os.path.exists('.svn'):
   print 'Error: must be run in the boost root directory'
   exit(1)
 print 'Boost root directory detected'
+
+if not os.path.exists('temp'):
+  print 'Create temp directory...'
+  os.mkdir('temp')
+  open('temp/boost-no-inspect', 'w').close()
 
 # cleanup clears locks or other residual problems (learned this the hard way!)
 print 'Clean working copy ...'
@@ -28,10 +46,17 @@ print 'Update working copy...'
 cmnd(['svn', 'up', '--non-interactive', '--trust-server-cert'])
 
 print 'Build inspect program...'
-old_dir = os.getcwd()
-os.chdir(['tools/inspect/build'])
-cmnd(['bjam'])
-os.chdir(old_dir)
+cmnd(['bjam', 'tools/inspect/build'])
 
 print 'Running inspect from ' + os.getcwd()
-cmnd(['dist/bin/inspect'])
+subprocess.call('dist/bin/inspect', stdout=open(filepath, 'w'))
+
+print 'Upload web page via ftp...'
+ftp = FTP(args.host, args.user, args.password)
+ftp.retrlines('LIST')
+ftp.storlines('STOR ' + args.filename, open(filepath)) 
+ftp.retrlines('LIST')
+ftp.quit()
+
+print "Inspect and upload complete"
+
