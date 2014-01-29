60f74fc79c814b0a04e7e323558ddec00800be57
K. Noel Belcourt <kbelco@sandia.gov>
2014-01-28 21:09:59 -0700

    Eliminate a git command by combining update and init.
    Strangely enough git submodule command allows you to pass
    the --init argument to submodule update.
    
    git submodule update --init
    
    We can eliminate one extraneous call to git, should slightly
    help regression testers.

--- tools/regression/src/regression.py
+++ tools/regression/src/regression.py
@@ -907,8 +907,7 @@ class runner:
                 self.git_command( 'remote', 'set-branches', '--add', 'origin',
                     branch)
                 self.git_command( 'pull', '--recurse-submodules' )
-                self.git_command( 'submodule', 'init')
-                self.git_command( 'submodule', 'update')
+                self.git_command( 'submodule', 'update', '--init' )
                 self.git_command( 'checkout', branch)
                 if clean:
                     self.git_command( 'reset', '--hard' )
