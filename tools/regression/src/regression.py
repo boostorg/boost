26449a7b63930c2e8a49cfe91301899c9647201c
Noel Belcourt <kbelco@sandia.gov>
2014-02-24 15:09:59 -0700

    Bump to 64 kb per test target.

--- tools/regression/src/regression.py
+++ tools/regression/src/regression.py
@@ -257,9 +257,9 @@ class runner:
             self.bjam_options += ' "--limit-tests=' + \
                 "|".join(lib for lib in self.libraries if lib != "build") + '"'
 
-        # if no -m bjam option add -m4 (limit target to 4kb of output)
+        # if no -m bjam option add -m64 (limit target to 64 kb of output)
         if self.bjam_options.find('-m') == -1:
-            self.bjam_options += ' -m4'
+            self.bjam_options += ' -m64'
 
         self.main()
 
