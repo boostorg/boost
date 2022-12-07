c874d58e174cc07daadd0e1593d16b82a1d68335
Lambourl <lmbrluc@gmail.com>
2022-12-07 10:42:06 +0100

    remove submodule ./libs/regex

---
+++ libs/regex/build/is_legacy_03.cpp
@@ -0,0 +1,16 @@
+/*
+ *
+ * Copyright (c) 2020
+ * John Maddock
+ *
+ * Use, modification and distribution are subject to the 
+ * Boost Software License, Version 1.0. (See accompanying file 
+ * LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ *
+ */
+
+#include <boost/regex/config.hpp>
+
+#ifdef BOOST_REGEX_CXX03
+#error "Legacy mode"
+#endif
